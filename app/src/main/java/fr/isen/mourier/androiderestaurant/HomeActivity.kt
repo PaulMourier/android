cc7aa1e50f10f3871639bbf1d6d16f04c72fba17
paulm <paul.mourier@isen.yncrea.fr>
2021-02-12 14:59:31 +0100

    boutons + toasts + new activity

--- app/src/main/java/fr/isen/mourier/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/mourier/androiderestaurant/HomeActivity.kt
@@ -1,7 +1,9 @@
 package fr.isen.mourier.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Toast
 import fr.isen.mourier.androiderestaurant.databinding.ActivityHomeBinding
 
@@ -14,6 +16,7 @@ class HomeActivity : AppCompatActivity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val monIntent : Intent = Intent(this, EntreesActivity::class.java)
 
         binding.entreesAction.setOnClickListener{
             //l'utilisateur a cliqué sur entrees
@@ -21,9 +24,11 @@ class HomeActivity : AppCompatActivity() {
             val duration = Toast.LENGTH_SHORT
             val toast = Toast.makeText(applicationContext, text, duration)
             toast.show()
+            startActivity(monIntent)
         }
         binding.platsAction.setOnClickListener{
             //l'utilisateur a cliqué sur plats
+
             val text = "Plats"
             val duration = Toast.LENGTH_SHORT
             val toast = Toast.makeText(applicationContext, text, duration)
