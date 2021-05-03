6ddcc4b2f0d427e0293072497585ec55b9513e30
paulm <paul.mourier@isen.yncrea.fr>
2021-05-03 11:18:47 +0200

    ble connection

--- app/src/main/java/fr/isen/mourier/androiderestaurant/DetailDeviceActivity.kt
+++ app/src/main/java/fr/isen/mourier/androiderestaurant/DetailDeviceActivity.kt
@@ -1,5 +1,9 @@
 package fr.isen.mourier.androiderestaurant
 
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import fr.isen.mourier.androiderestaurant.databinding.ActivityDetailDeviceBinding
@@ -7,20 +11,53 @@ import fr.isen.mourier.androiderestaurant.databinding.ActivityDetailDeviceBindin
 class DetailDeviceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailDeviceBinding
+    var bluetoothGatt: BluetoothGatt? = null
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailDeviceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         var deviceName = intent.getStringExtra("deviceName")
-        if(deviceName == null)
-        {
-            binding.titleDetailDevice.text = "No Name"
-        }
-        else
-        {
-            binding.titleDetailDevice.text = deviceName.toString()
-        }
+        val device = intent.getParcelableExtra<BluetoothDevice>("ble_device")
+        binding.titleDetailDevice.text = device?.name ?: "Device Unknown"
+        binding.statusDetailDevice.text =
+            getString(R.string.ble_device_status, getString(R.string.ble_device_status_connecting))
+
+        connectToDevice(device)
+    }
+
+    private fun connectToDevice(device: BluetoothDevice?) {
+        bluetoothGatt = device?.connectGatt(this, false, object : BluetoothGattCallback() {
+            override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+                super.onConnectionStateChange(gatt, status, newState)
+                connectionStateChange(newState, gatt)
+
+            }
+
+            override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+                super.onServicesDiscovered(gatt, status)
+            }
+
+            override fun onCharacteristicRead(
+                gatt: BluetoothGatt?,
+                characteristic: BluetoothGattCharacteristic?,
+                status: Int
+            ) {
+                super.onCharacteristicRead(gatt, characteristic, status)
+            }
+        })
+    }
+
+    private fun connectionStateChange(newState: Int, gatt: BluetoothGatt?) {
+        runOnUiThread {  BLEConnexionState.getBLEConnexionStateFromState(newState)
+            ?.let {//si non nul on fait la suite
+                binding.statusDetailDevice.text = getString(
+                    R.string.ble_device_status,
+                    getString(it.text)
+                )
+            }}
     }
 }
+
+
